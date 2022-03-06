a6b9974014c6bbfd3a1bac6cc210e1b103b0ac63
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-07 01:47:28 +0900

    Revert "feat: ascii85で文字化け対策して符号化"
    This reverts commit 0fefc4263df04d13b08066f9e1ebe69a6f375e6b.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::{fs::File, io::Read};
-
 use qrcode_generator::QrCodeEcc;
 
 fn main() {
-    code2();
-}
-
-fn code1() {
     qrcode_generator::to_svg_to_file("正直適正バチバチですよ。入部してみませんか？ https://oucrc.net/join/", QrCodeEcc::Low, 39, None::<&str>, "qrcode.svg").unwrap();
-}
-
-fn code2() {
-    let mut file = File::open("qrcode.repath.min.svg.bz2").unwrap();
-    let mut buf = Vec::new();
-    file.read_to_end(&mut buf).unwrap();
-    print!("{}", ascii85::encode(&buf));
 }
