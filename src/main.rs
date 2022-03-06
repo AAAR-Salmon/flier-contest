a26f39e7b84f1b35e6cc4ffa6fb0fb511b3df8b8
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2022-03-07 01:57:22 +0900

    feat: ascii85で文字化け対策しつつQRコード化

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::{fs::File, io::Read};
+
 use qrcode_generator::QrCodeEcc;
 
 fn main() {
+    code3()
+}
+
+fn code1() {
     qrcode_generator::to_svg_to_file("正直適正バチバチですよ。入部してみませんか？ https://oucrc.net/join/", QrCodeEcc::Low, 39, None::<&str>, "qrcode.svg").unwrap();
+}
+
+fn code3() {
+    let mut file = File::open("qrcode.repath.svgpath.bz2").unwrap();
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+    qrcode_generator::to_svg_to_file(ascii85::encode(&buf).as_bytes(), QrCodeEcc::Low, 101, None::<&str>, "qrcode.repath.svgpath.bz2.ascii85.svg").unwrap();
 }
